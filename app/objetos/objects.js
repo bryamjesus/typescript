4cb62770c55f8f445f220b3c55ad15c3a0e08fc6
Bryam Jesus Talledo Garcia <btalledog@odybank.com.pe>
2023-01-23 09:49:30 -0500

    Objetos

---
+++ app/objetos/objects.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allent',
+        age: 24,
+        powers: ['Súper velocidad', 'Viajat en el tiempo']
+    };
+    flash = {
+        name: 'Superman',
+        age: 60,
+        powers: ['Súper fuerza'],
+    };
+})();
---
+++ app/objetos/objects.ts
@@ -0,0 +1,15 @@
+(()=>{
+  let flash = {
+    name: 'Barry Allent',
+    age: 24,
+    powers: ['Súper velocidad', 'Viajat en el tiempo']
+  }
+
+  flash = {
+    name: 'Superman',
+    age:60,
+    powers:['Súper fuerza'],
+    
+  }
+
+})()
